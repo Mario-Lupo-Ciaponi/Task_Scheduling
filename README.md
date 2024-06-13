d8a65cb94b11eba27e9ebc9f4f38c1955b8b10b6
mariociaponi <mario.lupo.ciaponi08@gmail.com>
2024-06-13 14:13:57 +0300

    Added a new function and updated readme file.

--- README.md
+++ README.md
@@ -7,8 +7,9 @@ You can go to the next task only if you completed the current one.
 2. Mark task as completed. Removes the first task(the most important one) if the schedule is not empty.
 3. Show the most important task. Shows the most important task(the first one) if the schedule is not empty. If the schedule has only one task, it will it.
 4. Show the least important task. Shows the least important task(the last one) if the schedule is not empty. If the schedule has only one task, it will it.
-5. Show Task Schedule. Shows all the tasks if the schedule is not empty.
-6. Quit. It is self-explanatory
+5. View Task Schedule. Shows all the tasks if the schedule is not empty.
+6. Show completed tasks. If you completed any tasks, it will show you the tasks.
+7. Quit. It is self-explanatory
 
 ## Purpose:
 The purpose is to practice my the data structure queues(as a list), Git and GitHub skills.
--- task_scheduling.py
+++ task_scheduling.py
@@ -13,12 +13,13 @@ def add_tasks(tasks_queue, task):
     return tasks_queue
 
 
-def remove_task(tasks_queue):
+def remove_task(tasks_queue, completed_tasks):
     removed_task = tasks_queue.pop(0)
+    completed_tasks.append(removed_task)
 
     print(f"'{removed_task}' removed successfully!")
 
-    return tasks_queue
+    return tasks_queue, completed_tasks
 
 
 def most_important_task_in_schedule(task_queue):
@@ -41,13 +42,21 @@ def show_task_schedule(task_queue):
             print(f"{number} - {task_queue[number - 1]}")
 
 
+def print_completed_tasks(completed_tasks):
+    print("Completed tasks:")
+
+    for i in range(1, len(completed_tasks) + 1):
+        print(f"{i} - {completed_tasks[i - 1]}")
+
+
 def show_operations():
     print("\n/---Task Operations---\\")
     print("\t1) Add task\n\t2) Mark Task as completed\n\t3) Show the most important task"
-          "\n\t4) Show the least important task\n\t5) Show Task Schedule\n\t6) Quit\n")
+          "\n\t4) Show the least important task\n\t5) View Task Schedule\n\t6) View completed tasks "
+          "\n\t7) Quit\n")
 
 
-def task_manipulation(task_queue):
+def task_manipulation(task_queue, completed_tasks):
     print("Welcome to the Task Scheduling!")
 
     while True:
@@ -60,7 +69,7 @@ def task_manipulation(task_queue):
             task_queue = add_tasks(task_queue, task_to_add)
         elif option == 2:
             if not is_queue_empty(task_queue):
-                task_queue = remove_task(task_queue)
+                task_queue, completed_tasks = remove_task(task_queue, completed_tasks)
             else:
                 print("Task schedule is empty!")
         elif option == 3:
@@ -74,7 +83,7 @@ def task_manipulation(task_queue):
                 print(f"Most important task: {most_important_task}")
         elif option == 4:
             if len(task_queue) == 1:
-                print(f"You have only one task. Task: {task_queue[0]}")
+                print(f"\nYou have only one task. Task: {task_queue[0]}")
             elif len(task_queue) == 0:
                 print("\nNo tasks!")
             else:
@@ -87,6 +96,11 @@ def task_manipulation(task_queue):
             else:
                 print("\nNo tasks!")
         elif option == 6:
+            if completed_tasks:
+                print_completed_tasks(completed_tasks)
+            else:
+                print("\nYou have not completed any task.")
+        elif option == 7:
             print("\nGoodbye! Have a wonderful day!")
             break
         else:
@@ -95,8 +109,9 @@ def task_manipulation(task_queue):
 
 def main():
     task_queue = []
+    completed_tasks = []
 
-    task_manipulation(task_queue)
+    task_manipulation(task_queue, completed_tasks)
 
 
 if __name__ == "__main__":
